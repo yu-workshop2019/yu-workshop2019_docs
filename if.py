6e091ed2d0f68bb2f3d731790d7773566adbfaca
yu-workshop2019 <48100769+yu-workshop2019@users.noreply.github.com>
2019-04-05 10:55:40 +0900

    Add files via upload

---
+++ if.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+number = 1
+#number = 1
+print("numberには" + str(number) + "が代入されています")
+
+if (number == 1):
+    print("数字は1です")
+else:
+    print("数字は1ではありません")
