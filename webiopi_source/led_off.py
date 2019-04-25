76a09490e490e0fc0a2aefc35dc0270c6190e27c
yu-workshop2019 <48100769+yu-workshop2019@users.noreply.github.com>
2019-04-25 13:46:39 +0900

    Add files via upload

---
+++ webiopi_source/led_off.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import wiringpi as wp
+import time
+
+LED_PIN = 26
+
+wp.wiringPiSetupGpio()
+
+wp.pinMode(LED_PIN , 1)
+
+try:
+    wp.digitalWrite(LED_PIN, 0)
+except:
+    pass
