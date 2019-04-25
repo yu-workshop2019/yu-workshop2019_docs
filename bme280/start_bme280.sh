8cc943971a0561e38c68f88b0606de21042d169e
yu-workshop2019 <48100769+yu-workshop2019@users.noreply.github.com>
2019-04-25 14:55:59 +0900

    Update start_bme280.sh

--- bme280/start_bme280.sh
+++ bme280/start_bme280.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #set -eu
 sudo nohup python  -m SimpleHTTPServer 88 >/dev/null 2>&1 &
-sudo python /home/pi/webiopi_source/bme280/bme280.py &
-sudo /home/pi/webiopi_source/bme280/measure_temp.sh &
+sudo python /home/pi/bme280/bme280.py &
+sudo /home/pi/bme280/measure_temp.sh &
