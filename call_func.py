285ef11f35007d0b5de4fbc12eddc4f7deaff3e6
yu-workshop2019 <48100769+yu-workshop2019@users.noreply.github.com>
2019-05-07 23:34:17 +0900

    Add files via upload

---
+++ call_func.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#func.pyのインポート
+import func
+
+def main():
+
+    func.func()
+
+    x = 5
+    print(str(x) + "の2乗は" + str(func.pow(x)) + "です。")
+
+    a, b = func.rand()
+    print("2つの乱数は、" + str(a) + "と" +  str(b) + "です。")
+
+#プログラム実行時に実行される部分
+if __name__ == '__main__':
+    main()
